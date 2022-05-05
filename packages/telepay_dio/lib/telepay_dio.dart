e35be36f9cc93fb531e09c8810caa4f430b279b4
Luis Ciber <luisciber640@gmail.com>
2022-05-04 22:47:30 -0400

    perf: fix some things and upgrade mocktail dependencie

--- packages/telepay_dio/lib/telepay_dio.dart
+++ packages/telepay_dio/lib/telepay_dio.dart
@@ -2,5 +2,5 @@ library telepay_dio;
 
 export 'package:telepay/src/exceptions/exceptions.dart';
 export 'package:telepay/src/models/models.dart';
-export 'package:telepay/telepay.dart' hide TelePay;
+export 'package:telepay/telepay.dart';
 export 'src/telepay_dio.dart';
