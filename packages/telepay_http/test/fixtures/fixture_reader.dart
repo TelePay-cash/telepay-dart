02d99a9be9d2e9d069884fb4f109b409db5f7447
Luis Ciber <luisciber640@gmail.com>
2022-05-22 06:28:32 -0400

    chore(telepay_http): test and improve package

--- packages/telepay_http/test/fixtures/fixture_reader.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-String fixture(String name) {
-  var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
-  }
-  return File('$dir/test/fixtures/$name').readAsStringSync();
-}
